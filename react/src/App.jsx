06cda9a50ed0d4c09e1a09b3a0f91e44b1becfd7
Suraj Yakkha <yakkhasuraj@gmail.com>
2023-11-07 07:52:54 +0545

    feat: learn react lifecycle

--- react/src/App.jsx
+++ react/src/App.jsx
@@ -1,17 +1,41 @@
+import { useState } from "react";
 import { ClassComponent, FunctionComponent } from "./components";
 
 // Data flow top-down, one-way
 function App() {
+  const [display, setDisplay] = useState(false);
+  let a = "apple";
+  console.log(a, display);
+
   return (
     <>
-      <FunctionComponent>
+      {display ? (
         <ClassComponent>
           <h2>Children</h2>
         </ClassComponent>
-        <ClassComponent>
+      ) : (
+        <FunctionComponent
+          user={{ name: "John Doe", age: 10 }}
+          displayChildren={display}
+          country="Nepal"
+          cars={["Mustang", "Nexon"]}
+          salary={10_00_000}
+        >
+          {/* <ClassComponent>
           <h2>Children</h2>
-        </ClassComponent>
-      </FunctionComponent>
+        </ClassComponent> */}
+        </FunctionComponent>
+      )}
+
+      <button
+        onClick={() => {
+          setDisplay(!display);
+          a = "banana";
+          console.log(a);
+        }}
+      >
+        Display
+      </button>
     </>
   );
 }
--- react/src/components/function-component.jsx
+++ react/src/components/function-component.jsx
@@ -1,9 +1,46 @@
-export function FunctionComponent({ children }) {
-  return (
+import { useEffect, useState } from "react";
+
+export function FunctionComponent({
+  user /*  = { name: "Jane", age: 20 } */,
+  country,
+  cars,
+  salary,
+  children,
+  displayChildren,
+}) {
+  console.log(user, country, cars, salary);
+
+  const [count, setCount] = useState(1);
+
+  // if (displayChildren) {
+  //   return children;
+  // } else {
+  //   return (
+  //     <>
+  //       <h1>I&apos;m {user.name}</h1>
+  //       <h2>Count: {count}</h2>
+  //       <h1>Function Component</h1>
+  //       <button onClick={() => setCount(2)}>Change to 2</button>
+  //     </>
+  //   );
+  // }
+
+  // Mount, If dependency changed runs again, Unmount
+  useEffect(() => {
+    console.log("Effect run", count);
+    return () => {
+      console.log("Cleanup function call");
+    };
+  }, [count]);
+
+  return displayChildren ? (
+    children
+  ) : (
     <>
+      {user && <h1>I&apos;m {user.name}</h1>}
+      <h2>Count: {count}</h2>
       <h1>Function Component</h1>
-      {children}
-      <h1>Function Component</h1>
+      <button onClick={() => setCount(2)}>Change to 2</button>
     </>
   );
 }
